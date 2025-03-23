faa04cfbbe437c4b60f243232817bb9db662a108
Bhagyaj Kumar B <bhagyajkumar1@gmail.com>
2025-03-23 17:57:17 +0530

    exam in admin

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Class, ClassTeaching, Subject, ExamResult
+from .models import Class, ClassTeaching, Subject, ExamResult, Exam
 
 admin.site.register(Class)
 admin.site.register(ClassTeaching)
 admin.site.register(Subject)
 admin.site.register(ExamResult)
+admin.site.register(Exam)
